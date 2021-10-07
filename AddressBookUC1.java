ac061ee201f0b6065e82d0194ade3129c9979a12
Amit Sisodiya <amitsisodiya275@gmail.com>
2021-10-07 07:57:08 +0530

    Added: a new file for Address Book which can add contact to the Address Book.

---
+++ AddressBookUC1.java
@@ -0,0 +1,12 @@
+package com.bridgelab.addressbook;
+
+public class AddressBookUC1 {
+
+	public static void main(String[] args) {
+
+		Contact person1 = new Contact("Amit", "Sisodiy", "Mumbai", "Maharashtra", "example@gmail.com", 400071,
+				1234567890);
+		System.out.println(person1);
+	}
+
+}
